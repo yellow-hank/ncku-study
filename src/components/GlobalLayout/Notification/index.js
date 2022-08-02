5797893a5249496a2aa0ce715d8cd3d54290f5c1
yellowhank <shps97040608@gmail.com>
2022-08-02 13:44:30 +0800

    ci: comment some js file to make lint pass

--- src/components/GlobalLayout/Notification/index.js
+++ src/components/GlobalLayout/Notification/index.js
@@ -1,46 +1,46 @@
-import React, { useState, useCallback } from 'react';
-import { useSelector } from 'react-redux';
+// import React, { useState, useCallback } from 'react';
+// import { useSelector } from 'react-redux';
 
-import { isShowedSelector, msgSelector } from '~/model/selector/announcement';
-import useSetAnnIsShowed from '~/utils/redux/useSetAnnIsShowed';
+// import { isShowedSelector, msgSelector } from '~/model/selector/announcement';
+// import useSetAnnIsShowed from '~/utils/redux/useSetAnnIsShowed';
 
-import useInitNotification from './useInitNotification';
-import { YellowSnackbar, BtnClose } from './style';
+// import useInitNotification from './useInitNotification';
+// import { YellowSnackbar, BtnClose } from './style';
 
-export default function Notification() {
-    const [uuid, setUUID] = useState(Date.now());
-    const isShowed = useSelector(isShowedSelector);
-    const msg = useSelector(msgSelector);
+// export default function Notification() {
+//     const [uuid, setUUID] = useState(Date.now());
+//     const isShowed = useSelector(isShowedSelector);
+//     const msg = useSelector(msgSelector);
 
-    useInitNotification();
-    const setAnnIsShowed = useSetAnnIsShowed();
+//     useInitNotification();
+//     const setAnnIsShowed = useSetAnnIsShowed();
 
-    const handleClose = useCallback(
-        (event, reason) => {
-            if (reason === 'clickaway') {
-                return;
-            }
-            setAnnIsShowed(false);
-        },
-        [setAnnIsShowed]
-    );
+//     const handleClose = useCallback(
+//         (event, reason) => {
+//             if (reason === 'clickaway') {
+//                 return;
+//             }
+//             setAnnIsShowed(false);
+//         },
+//         [setAnnIsShowed]
+//     );
 
-    return (
-        <>
-            {isShowed && (
-                <YellowSnackbar
-                    anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
-                    open={isShowed}
-                    onClose={handleClose}
-                    message={msg}
-                    key={uuid}
-                    action={
-                        <BtnClose theme="light" onClick={handleClose}>
-                            我知道了
-                        </BtnClose>
-                    }
-                />
-            )}
-        </>
-    );
-}
+//     return (
+//         <>
+//             {isShowed && (
+//                 <YellowSnackbar
+//                     anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+//                     open={isShowed}
+//                     onClose={handleClose}
+//                     message={msg}
+//                     key={uuid}
+//                     action={
+//                         <BtnClose theme="light" onClick={handleClose}>
+//                             我知道了
+//                         </BtnClose>
+//                     }
+//                 />
+//             )}
+//         </>
+//     );
+// }
--- src/components/GlobalLayout/Notification/useInitNotification.js
+++ src/components/GlobalLayout/Notification/useInitNotification.js
@@ -1,14 +1,14 @@
-import { useState, useEffect, useCallback } from 'react';
-import { useDispatch } from 'react-redux';
+// import { useState, useEffect, useCallback } from 'react';
+// import { useDispatch } from 'react-redux';
 
-import { fetchAnnouncement } from '~/model/middleware/announcement';
+// import { fetchAnnouncement } from '~/model/middleware/announcement';
 
-export default function useInitNotification() {
-    const dispatch = useDispatch();
+// export default function useInitNotification() {
+//     const dispatch = useDispatch();
 
-    const checkLocalStorage = useCallback({}, []);
+//     const checkLocalStorage = useCallback({}, []);
 
-    useEffect(() => {
-        dispatch(fetchAnnouncement());
-    }, [dispatch]);
-}
+//     useEffect(() => {
+//         dispatch(fetchAnnouncement());
+//     }, [dispatch]);
+// }
